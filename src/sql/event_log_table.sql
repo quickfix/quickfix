5b1b0a8d53b09f12be41979533011f77d1ef1de5
Oren Miller <oren@quickfixengine.org>
2002-10-25 15:14:16 +0000

    added session information to tables

--- src/sql/event_log_table.sql
+++ src/sql/event_log_table.sql
@@ -4,6 +4,9 @@ DROP TABLE IF EXISTS event_log;
 
 CREATE TABLE event_log (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  beginstring CHAR(8) NOT NULL,
+  sendercompid VARCHAR(64) NOT NULL,
+  targetcompid VARCHAR(64) NOT NULL,
   text BLOB NOT NULL,
   PRIMARY KEY (id)
 );
--- src/sql/incoming_log_table.sql
+++ src/sql/incoming_log_table.sql
@@ -4,6 +4,9 @@ DROP TABLE IF EXISTS incoming_log;
 
 CREATE TABLE incoming_log (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  beginstring CHAR(8) NOT NULL,
+  sendercompid VARCHAR(64) NOT NULL,
+  targetcompid VARCHAR(64) NOT NULL,
   text BLOB NOT NULL,
   PRIMARY KEY (id)
 );
