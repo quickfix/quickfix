fc1c37512a9f72d437c2edf5a7b1a5bb13688f68
Oren Miller <oren@quickfixengine.org>
2003-03-03 06:35:01 +0000

    generates deprecated fields

--- spec/generate_java.bat
+++ spec/generate_java.bat
@@ -10,8 +10,14 @@ msxsl FIX40.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix40\Me
 msxsl FIX41.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix41\MessageCracker.java
 msxsl FIX42.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix42\MessageCracker.java
 msxsl FIX43.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix43\MessageCracker.java
-msxsl FIX43.xml generate_java_fields.xsl -o generate_java_fields.bat
-call generate_java_fields.bat
+msxsl FIX40.xml generate_java_fields.xsl -o generate_java_fields40.bat
+msxsl FIX41.xml generate_java_fields.xsl -o generate_java_fields41.bat
+msxsl FIX42.xml generate_java_fields.xsl -o generate_java_fields42.bat
+msxsl FIX43.xml generate_java_fields.xsl -o generate_java_fields43.bat
+call generate_java_fields40.bat
+call generate_java_fields41.bat
+call generate_java_fields42.bat
+call generate_java_fields43.bat
 msxsl FIX40.xml generate_java_messages.xsl -o generate_java_messages40.bat
 msxsl FIX41.xml generate_java_messages.xsl -o generate_java_messages41.bat
 msxsl FIX42.xml generate_java_messages.xsl -o generate_java_messages42.bat
