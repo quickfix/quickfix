5a2fc56f74c43150a5d5702f9211ce3ca2f81666
Oren Miller <oren@quickfixengine.org>
2003-03-03 06:52:39 +0000

    moved deprecated fields up one directory

---
+++ src/java/src/org/quickfix/field/ListNoOrds.java
@@ -0,0 +1,16 @@
+package org.quickfix.field; 
+import org.quickfix.IntField; 
+import java.util.Date; 
+
+public class ListNoOrds extends IntField 
+{ 
+
+  public ListNoOrds() 
+  { 
+    super(68);
+  } 
+  public ListNoOrds(int data) 
+  { 
+    super(68, data);
+  } 
+} 
