21385adfe298d9cbb0ef2e51e1b2b837380e897b
Oren Miller <oren@quickfixengine.org>
2003-05-08 01:50:27 +0000

    copy adheres to new directory structure

--- src/C++/copy.sh
+++ src/C++/copy.sh
@@ -1,6 +1,6 @@
 until [ -z "$1" ]
 do
-  bash ./copyifnotexist.sh $1 ../../include/$1
-  bash ./copyifnewer.sh $1 ../../include/$1
+  bash ./copyifnotexist.sh $1 ../../include/quickfix/$1
+  bash ./copyifnewer.sh $1 ../../include/quickfix/$1
   shift
 done
