c47e0d720e4a9885c56ac3d4d25e28ac39320ca7
Oren Miller <oren@quickfixengine.org>
2003-05-17 21:37:29 +0000

    check return values

--- test/check.sh
+++ test/check.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
+sleep 1
 
 QUICKFIX_TEST_PORT=5000
 
 ./runut $QUICKFIX_TEST_PORT
+RESULT=$?
+if [ $RESULT != 0 ]
+then 
+  exit $RESULT
+fi
+
+if [ $? != 0 ]
+then 
+  exit $?
+fi
+
 ./runat $QUICKFIX_TEST_PORT
+RESULT=$?
+if [ $RESULT != 0 ]
+then 
+  exit $RESULT
+fi
+
+sleep 1
