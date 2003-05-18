44aa63935f3f15168c99a314ee8706c4bdf36865
Oren Miller <oren@quickfixengine.org>
2003-05-18 17:57:34 +0000

    at build with ant

--- src/build.bat
+++ src/build.bat
@@ -1,4 +1,9 @@
-rem set up classpath
-set BUILD_CLASSPATH=..\lib\quickfix.jar
+@setlocal
 
-javac -classpath %BUILD_CLASSPATH% at_messagecracker.java at_application.java at.java
+REM set up classpath
+set CLASSPATH=../lib/ant.jar;../lib/optional.jar;../lib/junit.jar;../lib/crimson.jar;%JAVA_HOME%/lib/tools.jar
+
+REM call ant
+java org.apache.tools.ant.Main %*
+
+@endlocal
