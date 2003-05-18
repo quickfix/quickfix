4804799f3710f3fe809e7e1b59fd71c624c52b0e
Oren Miller <oren@quickfixengine.org>
2003-05-18 02:37:24 +0000

    creates cfg with setup.bat

--- test/runat.bat
+++ test/runat.bat
@@ -14,27 +14,7 @@ set EX=
 goto start
 
 :start
-echo [DEFAULT] > cfg\at.cfg
-echo ConnectionType=acceptor >> cfg\at.cfg
-echo SocketAcceptPort=%2 >> cfg\at.cfg
-echo SocketReuseAddress=Y >> cfg\at.cfg
-echo StartTime=00:00:00 >> cfg\at.cfg
-echo EndTime=00:00:00 >> cfg\at.cfg
-echo SenderCompID=ISLD >> cfg\at.cfg
-echo TargetCompID=TW >> cfg\at.cfg
-echo ResetOnDisconnect=Y >> cfg\at.cfg
-echo [SESSION] >> cfg\at.cfg
-echo BeginString=FIX.4.0 >> cfg\at.cfg
-echo DataDictionary=../spec/FIX40.xml >> cfg\at.cfg
-echo [SESSION] >> cfg\at.cfg
-echo BeginString=FIX.4.1 >> cfg\at.cfg
-echo DataDictionary=../spec/FIX41.xml >> cfg\at.cfg
-echo [SESSION] >> cfg\at.cfg
-echo BeginString=FIX.4.2 >> cfg\at.cfg
-echo DataDictionary=../spec/FIX42.xml >> cfg\at.cfg
-echo [SESSION] >> cfg\at.cfg
-echo BeginString=FIX.4.3 >> cfg\at.cfg
-echo DataDictionary=../spec/FIX43.xml >> cfg\at.cfg
+call setup.bat %2
 
 atrun -t run -s "..\bin\at%EX%.exe -f cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %2 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def" -i .\
 
--- test/runnewat.bat
+++ test/runnewat.bat
@@ -14,17 +14,7 @@ set EX=
 goto start
 
 :start
-echo [DEFAULT] > cfg\at.cfg
-echo ConnectionType=acceptor >> cfg\at.cfg
-echo SocketAcceptPort=%2 >> cfg\at.cfg
-echo StartTime=00:00:00 >> cfg\at.cfg
-echo EndTime=00:00:00 >> cfg\at.cfg
-echo ResetOnDisconnect=Y >> cfg\at.cfg
-echo [SESSION] >> cfg\at.cfg
-echo BeginString=FIX.4.2 >> cfg\at.cfg
-echo SenderCompID=ISLD >> cfg\at.cfg
-echo TargetCompID=TW >> cfg\at.cfg
-echo DataDictionary=../spec/FIX42.xml >> cfg\at.cfg
+call setup.bat
 
 atrun -t run -s "..\bin\at%EX%.exe -f cfg\at.cfg" -d . -c "ruby Runner.rb localhost %2 definitions\server\future\*.def" -i .
 goto quit
