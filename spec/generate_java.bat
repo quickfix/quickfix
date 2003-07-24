4ca6feff04495727a62a5fbe1008b991d8aecca6
Oren Miller <oren@quickfixengine.org>
2003-07-24 03:09:43 +0000

    added FIX 4.4

--- spec/generate_java.bat
+++ spec/generate_java.bat
@@ -2,27 +2,34 @@ msxsl FIX40.xml java_MessageFactory.xsl -o ..\src\java\src\org\quickfix\fix40\Me
 msxsl FIX41.xml java_MessageFactory.xsl -o ..\src\java\src\org\quickfix\fix41\MessageFactory.java
 msxsl FIX42.xml java_MessageFactory.xsl -o ..\src\java\src\org\quickfix\fix42\MessageFactory.java
 msxsl FIX43.xml java_MessageFactory.xsl -o ..\src\java\src\org\quickfix\fix43\MessageFactory.java
+msxsl FIX44.xml java_MessageFactory.xsl -o ..\src\java\src\org\quickfix\fix44\MessageFactory.java
 msxsl FIX40.xml java_Message.xsl -o ..\src\java\src\org\quickfix\fix40\Message.java
 msxsl FIX41.xml java_Message.xsl -o ..\src\java\src\org\quickfix\fix41\Message.java
 msxsl FIX42.xml java_Message.xsl -o ..\src\java\src\org\quickfix\fix42\Message.java
 msxsl FIX43.xml java_Message.xsl -o ..\src\java\src\org\quickfix\fix43\Message.java
+msxsl FIX44.xml java_Message.xsl -o ..\src\java\src\org\quickfix\fix44\Message.java
 msxsl FIX40.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix40\MessageCracker.java
 msxsl FIX41.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix41\MessageCracker.java
 msxsl FIX42.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix42\MessageCracker.java
 msxsl FIX43.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix43\MessageCracker.java
+msxsl FIX44.xml java_MessageCracker.xsl -o ..\src\java\src\org\quickfix\fix44\MessageCracker.java
 msxsl FIX40.xml generate_java_fields.xsl -o generate_java_fields40.bat
 msxsl FIX41.xml generate_java_fields.xsl -o generate_java_fields41.bat
 msxsl FIX42.xml generate_java_fields.xsl -o generate_java_fields42.bat
 msxsl FIX43.xml generate_java_fields.xsl -o generate_java_fields43.bat
+msxsl FIX44.xml generate_java_fields.xsl -o generate_java_fields44.bat
 call generate_java_fields40.bat
 call generate_java_fields41.bat
 call generate_java_fields42.bat
 call generate_java_fields43.bat
+call generate_java_fields44.bat
 msxsl FIX40.xml generate_java_messages.xsl -o generate_java_messages40.bat
 msxsl FIX41.xml generate_java_messages.xsl -o generate_java_messages41.bat
 msxsl FIX42.xml generate_java_messages.xsl -o generate_java_messages42.bat
 msxsl FIX43.xml generate_java_messages.xsl -o generate_java_messages43.bat
+msxsl FIX44.xml generate_java_messages.xsl -o generate_java_messages44.bat
 call generate_java_messages40.bat
 call generate_java_messages41.bat
 call generate_java_messages42.bat
 call generate_java_messages43.bat
+call generate_java_messages44.bat
--- spec/generate_net.bat
+++ spec/generate_net.bat
@@ -1,13 +1,16 @@
-msxsl FIX43.xml net_Fields.xsl -o ..\src\.NET\Fields.h
+msxsl FIX44.xml net_Fields.xsl -o ..\src\.NET\Fields.h
 msxsl FIX40.xml net_Messages.xsl -o ..\src\.NET\FIX40_Messages.h
 msxsl FIX41.xml net_Messages.xsl -o ..\src\.NET\FIX41_Messages.h
 msxsl FIX42.xml net_Messages.xsl -o ..\src\.NET\FIX42_Messages.h
 msxsl FIX43.xml net_Messages.xsl -o ..\src\.NET\FIX43_Messages.h
+msxsl FIX44.xml net_Messages.xsl -o ..\src\.NET\FIX44_Messages.h
 msxsl FIX40.xml net_MessageCracker.xsl -o ..\src\.NET\FIX40_MessageCracker.h
 msxsl FIX41.xml net_MessageCracker.xsl -o ..\src\.NET\FIX41_MessageCracker.h
 msxsl FIX42.xml net_MessageCracker.xsl -o ..\src\.NET\FIX42_MessageCracker.h
 msxsl FIX43.xml net_MessageCracker.xsl -o ..\src\.NET\FIX43_MessageCracker.h
+msxsl FIX44.xml net_MessageCracker.xsl -o ..\src\.NET\FIX44_MessageCracker.h
 msxsl FIX40.xml net_MessageFactory.xsl -o ..\src\.NET\FIX40_MessageFactory.h
 msxsl FIX41.xml net_MessageFactory.xsl -o ..\src\.NET\FIX41_MessageFactory.h
 msxsl FIX42.xml net_MessageFactory.xsl -o ..\src\.NET\FIX42_MessageFactory.h
 msxsl FIX43.xml net_MessageFactory.xsl -o ..\src\.NET\FIX43_MessageFactory.h
+msxsl FIX44.xml net_MessageFactory.xsl -o ..\src\.NET\FIX44_MessageFactory.h
