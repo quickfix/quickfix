679b33a2c41632d97ddaa46588990bafc47916d7
Oren Miller <oren@quickfixengine.org>
2003-08-01 16:25:16 +0000

    increased heap size of java to accomodate compilation

--- src/java/build.bat
+++ src/java/build.bat
@@ -4,6 +4,6 @@ REM set up classpath
 set CLASSPATH=../../lib/ant.jar;../../lib/optional.jar;../../lib/junit.jar;../../lib/crimson.jar;%JAVA_HOME%/lib/tools.jar
 
 REM call ant
-java org.apache.tools.ant.Main %*
+java -Xmx256m org.apache.tools.ant.Main %*
 
 @endlocal
