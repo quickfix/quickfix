ab88bb0120ca387ea7980f5a944cc0fb862a2a1c
Oren Miller <oren@quickfixengine.org>
2003-08-04 07:41:46 +0000

    added FIX 4.4

--- test/runat_java.bat
+++ test/runat_java.bat
@@ -2,7 +2,7 @@ echo off
 if "%1" == "" goto usage
 
 call setup.bat %1
-atrun -t run -s "java -Djava.library.path=..\lib -classpath ..\lib\quickfix.jar;..\lib\at.jar at -f cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %1 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def" -i .\
+atrun -t run -s "java -Djava.library.path=..\lib -classpath ..\lib\quickfix.jar;..\lib\at.jar at -f cfg\at.cfg" -d . -c "ruby Runner.rb 127.0.0.1 %1 definitions\server\fix40\*.def definitions\server\fix41\*.def definitions\server\fix42\*.def definitions\server\fix43\*.def definitions\server\fix44\*.def" -i .\
 
 goto quit
 
