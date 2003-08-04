ce35bb392e02b2a9917e98ddbe917cdb8cf7e44e
Oren Miller <oren@quickfixengine.org>
2003-08-04 06:28:27 +0000

    passes port to setup batch file

--- test/runnewat.bat
+++ test/runnewat.bat
@@ -14,7 +14,7 @@ set EX=
 goto start
 
 :start
-call setup.bat
+call setup.bat %2
 
 atrun -t run -s "..\bin\at%EX%.exe -f cfg\at.cfg" -d . -c "ruby Runner.rb localhost %2 definitions\server\future\*.def" -i .
 goto quit
