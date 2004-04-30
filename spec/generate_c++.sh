f549e6f097e32c1608d905f1cfef8b204187f1dd
Oren Miller <oren@quickfixengine.org>
2004-04-30 02:49:30 +0000

    re-added generation of message crackers

--- spec/generate_c++.sh
+++ spec/generate_c++.sh
@@ -1,4 +1,8 @@
 xsltproc -o ../src/C++/FieldNumbers.h FieldNumbers.xsl FIX44.xml 
 xsltproc -o ../src/C++/Fields.h Fields.xsl FIX44.xml 
 xsltproc -o ../src/C++/Values.h Values.xsl FIX44.xml 
-
+xsltproc -o ../src/C++/fix40/MessageCracker.h MessageCracker.xsl FIX40.xml
+xsltproc -o ../src/C++/fix41/MessageCracker.h MessageCracker.xsl FIX41.xml
+xsltproc -o ../src/C++/fix42/MessageCracker.h MessageCracker.xsl FIX42.xml
+xsltproc -o ../src/C++/fix43/MessageCracker.h MessageCracker.xsl FIX43.xml
+xsltproc -o ../src/C++/fix44/MessageCracker.h MessageCracker.xsl FIX44.xml
