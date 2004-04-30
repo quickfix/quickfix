24ab6804abcbd064a1baa5bbd596bdff174c492b
Oren Miller <oren@quickfixengine.org>
2004-04-30 02:33:07 +0000

    re-add generation of message crackers

--- spec/generate_c++.bat
+++ spec/generate_c++.bat
@@ -1,3 +1,8 @@
 msxsl FIX44.xml FieldNumbers.xsl -o ..\src\C++\FieldNumbers.h
 msxsl FIX44.xml Fields.xsl -o ..\src\C++\Fields.h
 msxsl FIX44.xml Values.xsl -o ..\src\C++\Values.h
+msxsl FIX40.xml MessageCracker.xsl -o ..\src\C++\fix40\MessageCracker.h
+msxsl FIX41.xml MessageCracker.xsl -o ..\src\C++\fix41\MessageCracker.h
+msxsl FIX42.xml MessageCracker.xsl -o ..\src\C++\fix42\MessageCracker.h
+msxsl FIX43.xml MessageCracker.xsl -o ..\src\C++\fix43\MessageCracker.h
+msxsl FIX44.xml MessageCracker.xsl -o ..\src\C++\fix44\MessageCracker.h
