83d4c4babf3fdd97e6ec37365edeb84e2e2626be
Oren Miller <oren@quickfixengine.org>
2004-06-15 15:15:53 +0000

    added back debug build

--- build_vc7.bat
+++ build_vc7.bat
@@ -2,22 +2,22 @@ echo off
 set COMMAND=%1
 if "%1" == "" set COMMAND=build
 
-rem devenv quickfix.sln /%COMMAND% Debug
-rem if ERRORLEVEL 1 goto quit
+devenv quickfix.sln /%COMMAND% Debug
+if ERRORLEVEL 1 goto quit
 
 devenv quickfix.sln /%COMMAND% Release
 if ERRORLEVEL 1 goto quit
 
-rem devenv quickfix_jni.sln /%COMMAND% Debug
-rem if ERRORLEVEL 1 goto quit
+devenv quickfix_jni.sln /%COMMAND% Debug
+if ERRORLEVEL 1 goto quit
 
 devenv quickfix_jni.sln /%COMMAND% Release
 if ERRORLEVEL 1 goto quit
 
-rem pushd examples
-rem devenv examples.sln /%COMMAND% Debug
-rem popd
-rem if ERRORLEVEL 1 goto quit
+pushd examples
+devenv examples.sln /%COMMAND% Debug
+popd
+if ERRORLEVEL 1 goto quit
 
 pushd examples
 devenv examples.sln /%COMMAND% Release
