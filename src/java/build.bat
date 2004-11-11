00913d9770c2f96f81150f4d16bb9076cb5aec9d
Oren Miller <oren@quickfixengine.org>
2004-11-11 17:12:36 +0000

    allocate extra memory inside ant script instead of when loading jvm

--- src/java/build.bat
+++ src/java/build.bat
@@ -4,6 +4,6 @@ REM set up classpath
 set CLASSPATH=../../lib/ant.jar;../../lib/optional.jar;../../lib/junit.jar;../../lib/crimson.jar;%JAVA_HOME%/lib/tools.jar
 
 REM call ant
-java -Xmx256m org.apache.tools.ant.Main %*
+java org.apache.tools.ant.Main %*
 
 @endlocal
