ad14a999334223eb047cd6b6bb9e55e8dc0a90e3
Oren Miller <oren@quickfixengine.org>
2005-03-31 15:48:29 +0000

    added ValidateUserDefinedFields configuration property

--- src/C++/SessionSettings.h
+++ src/C++/SessionSettings.h
@@ -56,6 +56,7 @@ static const std::string SOCKET_NODELAY = "SocketNodelay";
 static const std::string RECONNECT_INTERVAL = "ReconnectInterval";
 static const std::string VALIDATE_FIELDS_OUT_OF_ORDER = "ValidateFieldsOutOfOrder";
 static const std::string VALIDATE_FIELDS_HAVE_VALUES = "ValidateFieldsHaveValues";
+static const std::string VALIDATE_USER_DEFINED_FIELDS = "ValidateUserDefinedFields";
 static const std::string LOGON_TIMEOUT = "LogonTimeout";
 static const std::string FILE_STORE_PATH = "FileStorePath";
 static const std::string MYSQL_STORE_DATABASE = "MySQLStoreDatabase";
