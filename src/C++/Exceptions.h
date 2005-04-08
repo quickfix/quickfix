e2535cd4756f88e551831282d6a3e0e615c7c678
Oren Miller <oren@quickfixengine.org>
2005-04-08 21:49:33 +0000

    have windows format socket exception messages

--- src/C++/Exceptions.h
+++ src/C++/Exceptions.h
@@ -238,98 +238,11 @@ struct SocketException : public Exception
   std::string errorToWhat( int error )
   {
     this->error = error;
-
-    switch( error )
-    {
-    case WSAEINTR:
-      return "Bad address.";
-    case WSAEACCES:
-      return "Invalid argument.";
-    case WSAEFAULT:
-      return "Too many open files.";
-    case WSAEINVAL:
-      return "Resource temporarily unavailable.";
-    case WSAEWOULDBLOCK:
-      return "Resource temporarily unavailable.";
-    case WSAEINPROGRESS:
-      return "Operation now in progress.";
-    case WSAEALREADY:
-      return "Operation already in progress.";
-    case WSAENOTSOCK:
-      return "Socket operation on nonsocket.";
-    case WSAEDESTADDRREQ:
-      return "Destination address required.";
-    case WSAEMSGSIZE:
-      return "Message too long.";
-    case WSAEPROTOTYPE:
-      return "Protocol wrong type for socket.";
-    case WSAENOPROTOOPT:
-      return "Bad protocol option.";
-    case WSAEPROTONOSUPPORT:
-      return "Protocol not supported.";
-    case WSAESOCKTNOSUPPORT:
-      return "Socket type not supported.";
-    case WSAEOPNOTSUPP:
-      return "Operation not supported.";
-    case WSAEPFNOSUPPORT:
-      return "Protocol family not supported.";
-    case WSAEAFNOSUPPORT:
-      return "Address family not supported by protocol family.";
-    case WSAEADDRINUSE:
-      return "Address already in use.";
-    case WSAEADDRNOTAVAIL:
-      return "Cannot assign requested address.";
-    case WSAENETDOWN:
-      return "Network is down.";
-    case WSAENETUNREACH:
-      return "Network is unreachable.";
-    case WSAENETRESET:
-      return "Network dropped connection on reset.";
-    case WSAECONNABORTED:
-      return "Software caused connection abort.";
-    case WSAECONNRESET:
-      return "Connection reset by peer.";
-    case WSAENOBUFS:
-      return "No buffer space available.";
-    case WSAEISCONN:
-      return "Socket is already connected.";
-    case WSAENOTCONN:
-      return "Socket is not connected.";
-    case WSAESHUTDOWN:
-      return "Cannot send after socket shutdown.";
-    case WSAETIMEDOUT:
-      return "Connection timed out.";
-    case WSAECONNREFUSED:
-      return "Connection refused.";
-    case WSAEHOSTDOWN:
-      return "Host is down.";
-    case WSAEHOSTUNREACH:
-      return "No route to host.";
-    case WSAEPROCLIM:
-      return "Too many processes.";
-    case WSASYSNOTREADY:
-      return "Network subsystem is unavailable.";
-    case WSAVERNOTSUPPORTED:
-      return "Winsock.dll version out of range.";
-    case WSANOTINITIALISED:
-      return "Successful WSAStartup not yet performed.";
-    case WSAEDISCON:
-      return "Graceful shutdown in progress.";
-    case WSATYPE_NOT_FOUND:
-      return "Class type not found.";
-    case WSAHOST_NOT_FOUND:
-      return "Host not found.";
-    case WSATRY_AGAIN:
-      return "Nonauthoritative host not found.";
-    case WSANO_RECOVERY:
-      return "This is a nonrecoverable error.";
-    case WSANO_DATA:
-      return "Valid name, no data record of requested type.";
-    case WSASYSCALLFAILURE:
-      return "System call failure.";
-    default:
-      return "Unknown error";
-    }
+    char buffer[2048];
+    FormatMessage( FORMAT_MESSAGE_FROM_SYSTEM, NULL, error,
+                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                   buffer, 2048, NULL );
+    return buffer;
   }
 
   int error;
