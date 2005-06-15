c3c600b86a545f002b3cf1f468654ecbf6e21883
Oren Miller <oren@quickfixengine.org>
2005-06-15 02:53:11 +0000

    profile taken in as a parameter

--- build_vc6.bat
+++ build_vc6.bat
@@ -1,49 +1,30 @@
-echo off
+REM echo off
 set COMMAND=%1
 if "%1" == "" set COMMAND=BUILD
+if "%2" == "" set PROFILE=Release
 
-msdev quickfix.dsw /MAKE "at - Win32 Debug" /%COMMAND%
+msdev quickfix.dsw /MAKE "at - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "at - Win32 Release" /%COMMAND%
+msdev quickfix.dsw /MAKE "atrun - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "atrun - Win32 Debug" /%COMMAND%
+msdev quickfix.dsw /MAKE "pt - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "atrun - Win32 Release" /%COMMAND%
+msdev quickfix.dsw /MAKE "quickfix - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "pt - Win32 Debug" /%COMMAND%
+msdev quickfix.dsw /MAKE "ut - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "pt - Win32 Release" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "quickfix - Win32 Debug" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "quickfix - Win32 Release" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "ut - Win32 Debug" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix.dsw /MAKE "ut - Win32 Release" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix_jni.dsw /MAKE "quickfix_jni - Win32 Debug" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev quickfix_jni.dsw /MAKE "quickfix_jni - Win32 Release" /%COMMAND%
+msdev quickfix_jni.dsw /MAKE "quickfix_jni - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
 
 pushd examples
-msdev examples.dsw /MAKE "executor_cpp - Win32 Debug" /%COMMAND%
+msdev examples.dsw /MAKE "executor_cpp - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "executor_cpp - Win32 Release" /%COMMAND%
+
+msdev examples.dsw /MAKE "ordermatch - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "ordermatch - Win32 Debug" /%COMMAND%
+msdev examples.dsw /MAKE "ordermatch_ut - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "ordermatch - Win32 Release" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "ordermatch_ut - Win32 Debug" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "ordermatch_ut - Win32 Release" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "tradeclient - Win32 Debug" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "tradeclient - Win32 Release" /%COMMAND%
+msdev examples.dsw /MAKE "tradeclient - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
 popd
-
 :quit
--- build_vc7.bat
+++ build_vc7.bat
@@ -1,27 +1,14 @@
 echo off
 set COMMAND=%1
 if "%1" == "" set COMMAND=build
+if "%2" == "" set PROFILE=Release
 
-devenv quickfix.sln /%COMMAND% Debug
+devenv quickfix.sln /%COMMAND% %PROFILE%
 if ERRORLEVEL 1 goto quit
-
-devenv quickfix.sln /%COMMAND% Release
+devenv quickfix_jni.sln /%COMMAND% %PROFILE%
 if ERRORLEVEL 1 goto quit
-
-devenv quickfix_jni.sln /%COMMAND% Debug
-if ERRORLEVEL 1 goto quit
-
-devenv quickfix_jni.sln /%COMMAND% Release
-if ERRORLEVEL 1 goto quit
-
 pushd examples
-devenv examples.sln /%COMMAND% Debug
+devenv examples.sln /%COMMAND% %PROFILE%
 popd
 if ERRORLEVEL 1 goto quit
-
-pushd examples
-devenv examples.sln /%COMMAND% Release
-popd
-if ERRORLEVEL 1 goto quit
-
 :quit
