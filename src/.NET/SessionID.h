f559a5026616d702a8f4e7e8c97c7cf77d924a28
Oren Miller <oren@quickfixengine.org>
2005-07-05 15:48:37 +0000

    added accesor for session qualifier

--- src/.NET/SessionID.h
+++ src/.NET/SessionID.h
@@ -99,6 +99,12 @@ public:
     QF_STACK_CATCH
   }
 
+  String* getSessionQualifier() 
+  { QF_STACK_TRY 
+    return m_pUnmanaged->getSessionQualifier().c_str(); 
+    QF_STACK_CATCH 
+  } 
+
 private:
   FIX::SessionID* m_pUnmanaged;
 };
