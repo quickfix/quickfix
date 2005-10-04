aaae0a61a35a7d1403120a30254a15860e2a6ee0
Oren Miller <oren@quickfixengine.org>
2005-10-04 22:10:04 +0000

    remove absolute paths

--- src/sql/postgresql/create.sh
+++ src/sql/postgresql/create.sh
@@ -1,7 +1,7 @@
-/usr/local/pgsql/bin/dropdb -q quickfix
-/usr/local/pgsql/bin/createdb quickfix
-/usr/local/pgsql/bin/psql -d quickfix -f event_log_table.sql
-/usr/local/pgsql/bin/psql -d quickfix -f incoming_log_table.sql
-/usr/local/pgsql/bin/psql -d quickfix -f outgoing_log_table.sql
-/usr/local/pgsql/bin/psql -d quickfix -f messages_table.sql
-/usr/local/pgsql/bin/psql -d quickfix -f sessions_table.sql
+dropdb -q quickfix
+createdb quickfix
+psql -d quickfix -f event_log_table.sql
+psql -d quickfix -f incoming_log_table.sql
+psql -d quickfix -f outgoing_log_table.sql
+psql -d quickfix -f messages_table.sql
+psql -d quickfix -f sessions_table.sql
