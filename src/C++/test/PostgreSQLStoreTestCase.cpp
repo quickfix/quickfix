ef18bdda9b2b39b5e6d44e1966a2394dc3e6399c
Oren Miller <oren@quickfixengine.org>
2005-10-04 18:52:05 +0000

    initial checkin

---
+++ src/C++/test/PostgreSQLStoreTestCase.cpp
@@ -0,0 +1,84 @@
+/****************************************************************************
+** Copyright (c) 2001-2005 quickfixengine.org  All rights reserved.
+**
+** This file is part of the QuickFIX FIX Engine
+**
+** This file may be distributed under the terms of the quickfixengine.org
+** license as defined by quickfixengine.org and appearing in the file
+** LICENSE included in the packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.quickfixengine.org/LICENSE for licensing information.
+**
+** Contact ask@quickfixengine.org if any conditions of this licensing are
+** not clear to you.
+**
+****************************************************************************/
+
+#ifdef _MSC_VER
+#pragma warning( disable : 4503 4355 4786 )
+#include "stdafx.h"
+#else
+#include "config.h"
+#endif
+
+#ifdef HAVE_POSTGRESQL
+
+#include "PostgreSQLStoreTestCase.h"
+
+namespace FIX
+{
+bool PostgreSQLStoreTestCase::setGet::onSetup( MessageStore*& pObject )
+{
+  SessionID sessionID( BeginString( "FIX.4.2" ),
+                       SenderCompID( "SETGET" ), TargetCompID( "TEST" ) );
+
+  m_object = m_factory.create( sessionID );
+  pObject = &( *m_object );
+  pObject->reset();
+
+  return true;
+}
+
+void PostgreSQLStoreTestCase::setGet::onTeardown( MessageStore* pObject )
+{
+  m_factory.destroy( pObject );
+}
+
+bool PostgreSQLStoreTestCase::other::onSetup( MessageStore*& pObject )
+{
+  SessionID sessionID( BeginString( "FIX.4.2" ),
+                       SenderCompID( "SETGET" ), TargetCompID( "TEST" ) );
+
+  m_object = m_factory.create( sessionID );
+  pObject = &( *m_object );
+  pObject->reset();
+
+  return true;
+}
+
+void PostgreSQLStoreTestCase::other::onTeardown( MessageStore* pObject )
+{
+  m_factory.destroy( pObject );
+}
+
+bool PostgreSQLStoreTestCase::reload::onSetup( MessageStore*& pObject )
+{
+  SessionID sessionID( BeginString( "FIX.4.2" ),
+                       SenderCompID( "SETGET" ), TargetCompID( "TEST" ) );
+
+  m_object = m_factory.create( sessionID );
+  pObject = &( *m_object );
+
+  return true;
+}
+
+void PostgreSQLStoreTestCase::reload::onTeardown( MessageStore* pObject )
+{
+  m_factory.destroy( pObject );
+}
+} //namespace FIX
+
+#endif //HAVE_POSTGRESQL
---
+++ src/C++/test/PostgreSQLStoreTestCase.h
@@ -0,0 +1,89 @@
+/* -*- C++ -*- */
+
+/****************************************************************************
+** Copyright (c) 2001-2005 quickfixengine.org  All rights reserved.
+**
+** This file is part of the QuickFIX FIX Engine
+**
+** This file may be distributed under the terms of the quickfixengine.org
+** license as defined by quickfixengine.org and appearing in the file
+** LICENSE included in the packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.quickfixengine.org/LICENSE for licensing information.
+**
+** Contact ask@quickfixengine.org if any conditions of this licensing are
+** not clear to you.
+**
+****************************************************************************/
+
+#ifdef HAVE_POSTGRESQL
+#ifndef FIX_POSTGRESQLSTORETESTCASE_H
+#define FIX_POSTGRESQLSTORETESTCASE_H
+
+#include <CPPTest/TestCase.h>
+#include "MessageStoreTestCase.h"
+#include "PostgreSQLStore.h"
+#include "Utility.h"
+
+#include <stdio.h>
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif
+
+namespace FIX
+{
+class PostgreSQLStoreTestCase : public MessageStoreTestCase
+{
+public:
+  PostgreSQLStoreTestCase()
+  {
+    add( &m_setGet );
+    add( &m_other );
+    add( &m_reload );
+  }
+
+private:
+
+  class setGet : public MessageStoreTestCase::setGet
+  {
+  public:
+    setGet() : m_factory() {}
+    bool onSetup( MessageStore*& pObject );
+    void onTeardown( MessageStore* pObject );
+
+  private:
+    PostgreSQLStoreFactory m_factory;
+  }
+  m_setGet;
+
+  class other : public MessageStoreTestCase::other
+  {
+  public:
+    other() : m_factory() {}
+    bool onSetup( MessageStore*& pObject );
+    void onTeardown( MessageStore* pObject );
+
+  private:
+    PostgreSQLStoreFactory m_factory;
+  }
+  m_other;
+
+  class reload : public MessageStoreTestCase::reload
+  {
+  public:
+    reload() : m_factory() {}
+    bool onSetup( MessageStore*& pObject );
+    void onTeardown( MessageStore* pObject );
+
+  private:
+    PostgreSQLStoreFactory m_factory;
+  }
+  m_reload;
+};
+}
+
+#endif //FIX_POSTGRESQLSTORETESTCASE_H
+#endif //HAVE_POSTGRESQL
