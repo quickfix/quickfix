3c5642b0b55ef73340eb1ada70157359a4e8a95a
Oren Miller <oren@quickfixengine.org>
2005-10-21 10:04:49 +0000

    loadfiles from single script

--- src/sql/postgresql/create.sh
+++ src/sql/postgresql/create.sh
@@ -1,7 +1,3 @@
 dropdb -q quickfix
 createdb quickfix
-psql -d quickfix -f event_log_table.sql
-psql -d quickfix -f incoming_log_table.sql
-psql -d quickfix -f outgoing_log_table.sql
-psql -d quickfix -f messages_table.sql
-psql -d quickfix -f sessions_table.sql
+psql -d quickfix -f postgresql.sql
---
+++ src/sql/postgresql/postgresql.sql
@@ -0,0 +1,5 @@
+\i sessions_table.sql;
+\i messages_table.sql;
+\i incoming_log_table.sql;
+\i outgoing_log_table.sql;
+\i event_log_table.sql;
