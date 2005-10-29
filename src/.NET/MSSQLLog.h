70b8f45fd4fd2cc81b3ef39f8d3cb9083cbb4b6e
Oren Miller <oren@quickfixengine.org>
2005-10-29 07:24:47 +0000

    added MSSQL and PostgreSQL support to .NET

---
+++ src/.NET/MSSQLLog.h
@@ -0,0 +1,75 @@
+/* -*- C++ -*- */
+
+/****************************************************************************
+** Copyright (c) 2001-2005 quickfixengine.org  All rights reserved.
+**
+** This file is part of the QuickFIX FIX Engine
+**
+** This file may be distributed under the terms of the quickfixengine.org
+** license as defined by quickfixengine.org and appearing in the file
+** LICENSE included in the packaging of this file.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.quickfixengine.org/LICENSE for licensing information.
+**
+** Contact ask@quickfixengine.org if any conditions of this licensing are
+** not clear to you.
+**
+****************************************************************************/
+
+#ifdef HAVE_MSSQL
+#pragma once
+
+using namespace System;
+
+#include "quickfix_net.h"
+
+#include "CPPLog.h"
+#include "LogFactory.h"
+#include "MessageStore.h"
+#include "SessionID.h"
+#include "SessionSettings.h"
+#include "quickfix/MSSQLLog.h"
+#include "quickfix/Settings.h"
+#include "quickfix/CallStack.h"
+#include "vcclr.h"
+
+namespace QuickFix
+{
+public __gc class MSSQLLog : public CPPLog
+{
+public:
+  MSSQLLog( FIX::Log* pUnmanaged )
+  {
+    m_pUnmanaged = pUnmanaged;
+  }
+
+  ~MSSQLLog() { delete m_pUnmanaged; }
+};
+
+public __gc class MSSQLLogFactory : public LogFactory
+{
+public:
+  MSSQLLogFactory( SessionSettings* settings )
+  { QF_STACK_TRY
+    m_pUnmanaged = new FIX::MSSQLLogFactory(settings->unmanaged());
+    QF_STACK_CATCH
+  }
+
+  ~MSSQLLogFactory() { delete m_pUnmanaged; }
+
+  Log* create( SessionID* sessionID )
+  {
+    QF_STACK_TRY
+    return new MSSQLLog(m_pUnmanaged->create(sessionID->unmanaged()));
+    QF_STACK_CATCH
+  }
+
+private:
+  FIX::MSSQLLogFactory* m_pUnmanaged;
+};
+}
+
+#endif //HAVE_MSSQL
