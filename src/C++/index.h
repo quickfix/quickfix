d9a1317f029ca5a47624c75e2582d8a0b8fd8674
Oren Miller <oren@quickfixengine.org>
2005-11-26 01:33:45 +0000

    reorganized documentation

--- src/C++/index.h
+++ src/C++/index.h
@@ -1,30 +1,44 @@
 /*! \mainpage QuickFIX Documentation
-* <hr>
-* \section about About
-* <a href="license.html">License</a><br>
+* <table width="100%">
+* <tr>
+* <td><b>About</b></td>
+* <td><b>Installation</b></td>
+* <td><b>Getting Started</b></td>
+* <td><b>Working With Messages</b></td>
+* <td><b>Testing</b></td>
+* </tr>
+* <tr>
+* <td valign='top'>
 * <a href="about.html">About Quickfix</a><br>
-* <a href="FAQ.html">FAQ</a><br>
+* <a href="license.html">License</a><br>
 * <a href="http://sourceforge.net/mail/?group_id=37535">Mailing Lists</a><br>
-* \section start Getting Started
-* <a href="install.html">Installation</a><br>
+* <a href="FAQ.html">FAQ</a><br>
+* </td>
+* <td valign='top'>
+* <a href="dependencies.html">Dependencies</a><br>
+* <a href="building.html">Building QuickFIX</a><br>
+* <a href="generating_databases.html">Generating Databases</a><br>
+* <a href="testing.html">Testing QuickFIX</a><br>
+* </td>
+* <td valign='top'>
 * <a href="project.html">Setting Up Your Project</a><br>
 * <a href="application.html">Creating Your Application</a><br>
 * <a href="configuration.html">Configuration</a><br>
+* <a href="xmlschema.html">Validation</a><br>
+* </td>
+* <td valign='top'>
 * <a href="receiving_messages.html">Receiving Messages</a><br>
 * <a href="sending_messages.html">Sending Messages</a><br>
 * <a href="repeating_groups.html">Repeating Groups</a><br>
 * <a href="user_defined.html">User Defined Fields</a><br>
-* <a href="xmlschema.html">Validation</a><br>
 * <a href="examples.html">Example Applications</a><br>
-* \section source Source Code
-* <a href="files.html">Source Files</a><br>
-* <a href="annotated.html">Annotated Class List</a><br>
-* <a href="classes.html">Alphabetical Class List</a><br>
-* <a href="hierarchy.html">Class Hierarchy</a><br>
-* <a href="inherits.html">Graphical Class Hierarchy</a><br>
-* \section testing Testing
+* </td>
+* <td valign='top'>
 * <a href="unit_tests.html">Unit Tests</a><br>
 * <a href="acceptance_tests.html">Acceptance Tests</a><br>
+* </td>
+* </tr>
+* </table>
 *
 * \author Oren Miller oren@quickfixengine.org
 */
