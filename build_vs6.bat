a5f2ee281c2233aa6856f6951d1b4a254d3d735c
Oren Miller <oren@quickfixengine.org>
2006-01-15 22:56:29 +0000

    updated project and workspace name

--- build_vs6.bat
+++ build_vs6.bat
@@ -1,30 +1,26 @@
 echo off
 set COMMAND=%1
+set PROFILE=%2
 if "%1" == "" set COMMAND=BUILD
 if "%2" == "" set PROFILE=Release
 
-msdev quickfix_vs6.dsw /MAKE "at_vs6 - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "test_acceptance_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix_vs6.dsw /MAKE "atrun_vs6 - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "test_acceptance_runner_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix_vs6.dsw /MAKE "pt_vs6 - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "test_performance_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
 msdev quickfix_vs6.dsw /MAKE "quickfix_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev quickfix_vs6.dsw /MAKE "ut_vs6 - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "test_unit_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
 msdev quickfix_vs6.dsw /MAKE "quickfix_jni_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-
-pushd examples
-msdev examples.dsw /MAKE "executor_cpp - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "example_executor_cpp_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-
-msdev examples.dsw /MAKE "ordermatch - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "example_ordermatch_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "ordermatch_ut - Win32 %PROFILE%" /%COMMAND%
-if ERRORLEVEL 1 goto quit
-msdev examples.dsw /MAKE "tradeclient - Win32 %PROFILE%" /%COMMAND%
+msdev quickfix_vs6.dsw /MAKE "example_tradeclient_vs6 - Win32 %PROFILE%" /%COMMAND%
 if ERRORLEVEL 1 goto quit
 popd
 :quit
