c53197b5956e5e5af44bf5e85699e658830d80b4
Oren Miller <oren@quickfixengine.org>
2006-01-19 19:25:29 +0000

    removed unused files

--- testcasegen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CLASS=$1
-HEADERFILE=$1TestCase.h
-SOURCEFILE=$1TestCase.cpp
-ruby templategen.rb templates/template_TestCase.h $HEADERFILE "<CLASS>" $1 
-ruby templategen.rb templates/template_TestCase.cpp $SOURCEFILE "<CLASS>" $1 
