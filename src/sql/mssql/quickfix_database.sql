d966cbcc102c04e469d60d3548793847c767e226
Oren Miller <oren@quickfixengine.org>
2006-09-22 22:03:20 +0000

    Session qualifier can be null in log table

--- src/sql/mssql/quickfix_database.sql
+++ src/sql/mssql/quickfix_database.sql
@@ -42,7 +42,7 @@ CREATE TABLE messages_log (
   beginstring CHAR(8) NOT NULL,
   sendercompid VARCHAR(64) NOT NULL,
   targetcompid VARCHAR(64) NOT NULL,
-  session_qualifier VARCHAR(64) NOT NULL,
+  session_qualifier VARCHAR(64),
   text TEXT NOT NULL,
   PRIMARY KEY (id)
 );
