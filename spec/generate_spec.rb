c0cd3dd6bc0f28c9f7bb88f062f8d33b7a91a55a
Oren Miller <oren@quickfixengine.org>
2008-05-16 15:04:43 +0000

    Parse messages, remove output code

--- spec/generate_spec.rb
+++ spec/generate_spec.rb
@@ -4,11 +4,13 @@ class DataDictionary
 	def initialize( major, minor )
 		@major = major
 		@minor = minor
-		@tabs = 0
 
 		directory = "FIX.#{major}.#{minor}"
 		@fieldsDoc = REXML::Document.new( File.new( "#{directory}/Fields.xml" ) )
 		@enumsDoc = REXML::Document.new( File.new( "#{directory}/Enums.xml" ) )
+		@msgContentsDoc = REXML::Document.new( File.new("#{directory}/MsgContents.xml") )
+		@msgTypeDoc = REXML::Document.new( File.new("#{directory}/MsgType.xml") )
+		@componentsDoc =  REXML::Document.new( File.new("#{directory}/Components.xml") )
 
 		@specDoc = REXML::Document.new()
 
@@ -28,6 +30,15 @@ class DataDictionary
 
 	def toDescription( description )
 		old = description.clone
+
+		#while (pos = description =~ /[a-z][A-Z]/) != nil
+		#	description.insert( pos + 1, ' ' )
+		#end
+
+		#while (pos = description =~ /[A-Z][A-Z]/) != nil
+		#	description.insert( pos + 1, ' ' )
+		#end
+
 		description.upcase!
 		description = description.split("(")[0]
 		description = description.split(",")[0]
@@ -38,7 +49,7 @@ class DataDictionary
 		description.gsub!('/', '_')
 		description.gsub!('+', 'PLUS')
 		description = description.split("___")[0]
-		return description
+		return description + " | " + old
 	end
 
 	def toType( type )
@@ -88,9 +99,30 @@ class DataDictionary
 		}
 	end
 
+	def parseMsgType
+		@msgIdToMsgType = Hash.new
+
+		@msgTypeDoc.elements["dataroot"].elements.each("MsgType") { |msgTypeElement|
+			msgId = msgTypeElement.elements["MsgID"].text.to_i
+			msgType = msgTypeElement.elements["MsgType"].text
+			messageName = msgTypeElement.elements["MessageName"].text
+			componentType = msgTypeElement.elements["ComponentType"].text
+			category = msgTypeElement.elements["Category"].text
+
+			category = category == "Session" ? "admin" : "app"
+
+			msgTypeHash = Hash.new
+ 			msgTypeHash[ "msgtype" ] = msgType
+			msgTypeHash[ "name" ] = messageName
+			msgTypeHash[ "msgcat" ] = category
+			@msgIdToMsgType[ msgId ] = msgTypeHash
+		}
+	end
+
 	def parseDictionary
 		parseFields
 		parseEnums
+		parseMsgType
 	end
 
 	def printVersion
@@ -98,14 +130,10 @@ class DataDictionary
 	end
 
 	def printFields
-		tabUp
-		outs( '<fields>' )
-
-		tabUp
 		@tagToField.sort.each { |tag, fieldHash|
 			type = fieldHash[ "type" ]
 			fieldName = fieldHash[ "fieldName" ]
-			fieldElement = @fixElement.add_element( "field", { "field" => tag, "name" => fieldName, "type" => type } )
+			fieldElement = @fixElement.add_element( "field", { "field" => tag.to_s, "name" => fieldName, "type" => type } )
 			printEnums( fieldElement, tag )
 		}
 	end
@@ -113,35 +141,28 @@ class DataDictionary
 	def printEnums( fieldElement, tag )
 		return if !@tagToEnum.has_key?( tag )
 
-		tabUp
-			enumHash = @tagToEnum[ tag ]
- 			enumHash.sort.each { |enum, description|
-				fieldElement.add_element( "value", { "enum" => enum, "description" => description } )
-			}
-		tabDown
+		enumHash = @tagToEnum[ tag ]
+ 		enumHash.sort.each { |enum, description|
+			fieldElement.add_element( "value", { "enum" => enum, "description" => description } )
+		}
+	end
+
+	def printMessages
+		messagesElement = @fixElement.add_element( "messages" )
+		@msgIdToMsgType.sort.each { |msgId, msgTypeHash|
+			name = msgTypeHash[ "name" ]
+			msgtype = msgTypeHash[ "msgtype" ]
+			msgcat = msgTypeHash[ "msgcat" ]
+			messagesElement.add_element( "message", { "name" => name, "msgtype" => msgtype, "msgcat" => msgcat } )
+		}
 	end
 
 	def printDictionary
 		@fixElement = printVersion
 		printFields
+		printMessages
 		@specDoc.write( $stdout, 1, false, true )
-	end
-
-	def tabUp
-		@tabs += 1
-	end
-
-	def tabDown
-		@tabs -= 1
-	end
-
-	def outs( string )
-		for i in 1..@tabs 
-			print "  " if @tabs > 0
-		end
-
-		puts string
 	end
 end
 
-DataDictionary.new( 4, 0 )
+DataDictionary.new( 5, 0 )
