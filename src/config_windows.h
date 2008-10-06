b0806888d2ae51eee32116f33fcda50793574664
Oren Miller <oren@quickfixengine.org>
2008-10-06 21:06:00 +0000

    Commented out dependencies

--- src/config_windows.h
+++ src/config_windows.h
@@ -7,16 +7,16 @@
 // #define HAVE_STLPORT
 
 // Define if you want to build the java JNI wrapper
-#define HAVE_JAVA 1
+// #define HAVE_JAVA 1
 
 // Define if you have sql library (libMySQL.lib)
-#define HAVE_MYSQL 1
+// #define HAVE_MYSQL 1
 
 // Define if you have sql library (libpq.lib)
-#define HAVE_POSTGRESQL 1
+// #define HAVE_POSTGRESQL 1
 
 // Define if you have odbc library (Odbc32.lib)
-#define HAVE_ODBC 1
+// #define HAVE_ODBC 1
 
 // Define if you have libxml library
 // #define HAVE_LIBXML 1
