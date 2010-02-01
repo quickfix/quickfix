6d82d34218fa279a0c2ebf09609f3bd5b7258022
Oren Miller <oren@quickfixengine.org>
2010-02-01 23:29:59 +0000

    Started working on java fix50 support

--- spec/Generator.rb
+++ spec/Generator.rb
@@ -9,8 +9,8 @@ def generateVersion( type, major, minor )
   dir = "../src"
   xml = "#{type}#{major}#{minor}.xml"
   #Processor.new(xml, "#{dir}/C++", GeneratorCPP.new(type, major.to_s, minor.to_s, "#{dir}/C++"))
-  #Processor.new(xml, "#{dir}/java", GeneratorJava.new(type, major.to_s, minor.to_s, "#{dir}/java"))
-  Processor.new(xml, "#{dir}/.NET", GeneratorNET.new(type, major.to_s, minor.to_s, "#{dir}/.NET"))
+  Processor.new(xml, "#{dir}/java", GeneratorJava.new(type, major.to_s, minor.to_s, "#{dir}/java"))
+  #Processor.new(xml, "#{dir}/.NET", GeneratorNET.new(type, major.to_s, minor.to_s, "#{dir}/.NET"))
   #Processor.new(xml, "#{dir}/python", GeneratorPython.new(type, major.to_s, minor.to_s, "#{dir}/python"))
   #Processor.new(xml, "#{dir}/ruby", GeneratorRuby.new(type, major.to_s, minor.to_s, "#{dir}/ruby"))
 end
--- spec/generate_java.bat
+++ spec/generate_java.bat
@@ -1,21 +1,21 @@
-msxsl FIX40.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix40\MessageFactory.java
-msxsl FIX41.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix41\MessageFactory.java
-msxsl FIX42.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix42\MessageFactory.java
-msxsl FIX43.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix43\MessageFactory.java
-msxsl FIX44.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix44\MessageFactory.java
-msxsl FIX50.xml java_MessageFactory.xsl -o ..\src\java\src\quickfix\fix50\MessageFactory.java
-msxsl FIX40.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix40\MessageCracker.java
-msxsl FIX41.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix41\MessageCracker.java
-msxsl FIX42.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix42\MessageCracker.java
-msxsl FIX43.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix43\MessageCracker.java
-msxsl FIX44.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix44\MessageCracker.java
-msxsl FIX50.xml java_MessageCracker.xsl -o ..\src\java\src\quickfix\fix50\MessageCracker.java
-msxsl FIX40.xml generate_java_fields.xsl -o generate_java_fields40.bat
-msxsl FIX41.xml generate_java_fields.xsl -o generate_java_fields41.bat
-msxsl FIX42.xml generate_java_fields.xsl -o generate_java_fields42.bat
-msxsl FIX43.xml generate_java_fields.xsl -o generate_java_fields43.bat
-msxsl FIX44.xml generate_java_fields.xsl -o generate_java_fields44.bat
-msxsl FIX50.xml generate_java_fields.xsl -o generate_java_fields50.bat
+xsltproc -o ..\src\java\src\quickfix\fix40\MessageFactory.java java_MessageFactory.xsl FIX40.xml
+xsltproc -o ..\src\java\src\quickfix\fix41\MessageFactory.java java_MessageFactory.xsl FIX41.xml
+xsltproc -o ..\src\java\src\quickfix\fix42\MessageFactory.java java_MessageFactory.xsl FIX42.xml
+xsltproc -o ..\src\java\src\quickfix\fix43\MessageFactory.java java_MessageFactory.xsl FIX43.xml
+xsltproc -o ..\src\java\src\quickfix\fix44\MessageFactory.java java_MessageFactory.xsl FIX44.xml
+xsltproc -o ..\src\java\src\quickfix\fix50\MessageFactory.java java_MessageFactory.xsl FIX50.xml
+xsltproc -o ..\src\java\src\quickfix\fix40\MessageCracker.java java_MessageCracker.xsl FIX40.xml
+xsltproc -o ..\src\java\src\quickfix\fix41\MessageCracker.java java_MessageCracker.xsl FIX41.xml
+xsltproc -o ..\src\java\src\quickfix\fix42\MessageCracker.java java_MessageCracker.xsl FIX42.xml
+xsltproc -o ..\src\java\src\quickfix\fix43\MessageCracker.java java_MessageCracker.xsl FIX43.xml
+xsltproc -o ..\src\java\src\quickfix\fix44\MessageCracker.java java_MessageCracker.xsl FIX44.xml
+xsltproc -o ..\src\java\src\quickfix\fix50\MessageCracker.java java_MessageCracker.xsl FIX50.xml
+xsltproc -o generate_java_fields40.bat generate_java_fields.xsl FIX40.xml
+xsltproc -o generate_java_fields41.bat generate_java_fields.xsl FIX41.xml
+xsltproc -o generate_java_fields42.bat generate_java_fields.xsl FIX42.xml
+xsltproc -o generate_java_fields43.bat generate_java_fields.xsl FIX43.xml
+xsltproc -o generate_java_fields44.bat generate_java_fields.xsl FIX44.xml
+xsltproc -o generate_java_fields50.bat generate_java_fields.xsl FIX50.xml
 call generate_java_fields40.bat
 call generate_java_fields41.bat
 call generate_java_fields42.bat
