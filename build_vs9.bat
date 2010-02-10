cd31aee3179d59222b15280fa922bec970f2f6fc
Oren Miller <oren@quickfixengine.org>
2010-02-10 02:06:13 +0000

    Added vs9 and vs10 builds submitted by Jason Aubrey

--- build_vs9.bat
+++ build_vs9.bat
@@ -4,7 +4,7 @@ set PROFILE=%2
 set VSPATH=%3
 if "%1" == "" set COMMAND=build
 if "%2" == "" set PROFILE=Release
-if "%3" == "" set VSPATH=C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\
-"%VSPATH%devenv" quickfix_vs8.sln /%COMMAND% %PROFILE%
+if "%3" == "" set VSPATH=C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\
+"%VSPATH%devenv" quickfix_vs9.sln /%COMMAND% %PROFILE%
 if ERRORLEVEL 1 goto quit
 :quit
--- src/FieldTest.cs
+++ src/FieldTest.cs
@@ -6,14 +6,14 @@ using QuickFix;
 [TestFixture]
 public class FieldTestCase
 {
-	[Test]
-	public void testMessageSetGetString()
-	{
-		Side side1 = new Side( Side.BUY );
-		Side side2 = new Side( Side.BUY );
-		Assert.AreEqual( side1, side2 );
-		side2 = new Side();
-		side2.setValue( Side.BUY );
-		Assert.AreEqual( side1, side2 );
-	}
+  [Test]
+  public void testMessageSetGetString()
+  {
+    Side side1 = new Side( Side.BUY );
+    Side side2 = new Side( Side.BUY );
+    Assert.AreEqual( side1, side2 );
+    side2 = new Side();
+    side2.setValue( Side.BUY );
+    Assert.AreEqual( side1, side2 );
+  }
 }
