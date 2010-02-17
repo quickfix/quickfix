f6a88ff20825ba0dc7896746579316b955e6e3ce
Oren Miller <oren@quickfixengine.org>
2010-02-17 04:50:43 +0000

    Updated configuration documentation for FIX.5.0

--- doc/document.sh
+++ doc/document.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-WHICH_JAVADOC=$(which javadoc)
-if [ $? == 0 ] ; then
-    javadoc -D html/javadoc ../src/java/src/quickfix/*.java
-fi
-
 WHICH_DOXYGEN=$(which doxygen)
 if [ $? == 0 ] ; then
     doxygen
