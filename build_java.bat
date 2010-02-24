b480da64f31d55c5a7ddb56a99f1c30600f8d0fc
Oren Miller <oren@quickfixengine.org>
2010-02-24 03:41:40 +0000

    Removed executor build from batch file

--- build_java.bat
+++ build_java.bat
@@ -6,11 +6,6 @@ call build.bat %COMMAND%
 popd
 if ERRORLEVEL 1 goto quit
 
-pushd examples\executor\java
-call build.bat %COMMAND%
-popd
-if ERRORLEVEL 1 goto quit
-
 pushd examples\tradeclientgui\banzai
 call build.bat %COMMAND%
 popd
