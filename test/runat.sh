8dd0116a903f1c7b1ae1bf64f82bb249551fa694
Oren Miller <oren@quickfixengine.org>
2010-03-24 15:45:21 +0000

    Passing FIX50 sp2 tests, run tests for all versions, need separate runs for different service packs

--- test/runat.sh
+++ test/runat.sh
@@ -4,10 +4,26 @@ DIR=`pwd`
 PORT=$1
 ./setup.sh $PORT
 
+./at -f cfg/at.cfg &
+PROCID=$!
+cd $DIR
+ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix4*/*.def definitions/server/fix50/*.def
+RESULT1=$?
+kill $PROCID
+
 ./at -f cfg/atsp1.cfg &
 PROCID=$!
 cd $DIR
 ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp1/*.def
-RESULT=$?
+RESULT2=$?
 kill $PROCID
+
+./at -f cfg/atsp2.cfg &
+PROCID=$!
+cd $DIR
+ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp2/*.def
+RESULT3=$?
+kill $PROCID
+
+RESULT=$(( $RESULT1 + $RESULT2 ++ $RESULT3 ))
 exit $RESULT
