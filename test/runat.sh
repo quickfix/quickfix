949c9fce73c20ecad631a6e534ae1766cf05e8ab
Mike Gatny <mgatny@connamara.com>
2013-05-09 20:26:49 +0000

    Make compatible with ruby1.9

--- test/runat.sh
+++ test/runat.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+RUBY="ruby -I."
 DIR=`pwd`
 PORT=$1
 ./setup.sh $PORT
@@ -7,21 +8,21 @@ PORT=$1
 ./at -f cfg/at.cfg &
 PROCID=$!
 cd $DIR
-ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix4*/*.def definitions/server/fix50/*.def
+$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix4*/*.def definitions/server/fix50/*.def
 RESULT1=$?
 kill $PROCID
 
 ./at -f cfg/atsp1.cfg &
 PROCID=$!
 cd $DIR
-ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp1/*.def
+$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp1/*.def
 RESULT2=$?
 kill $PROCID
 
 ./at -f cfg/atsp2.cfg &
 PROCID=$!
 cd $DIR
-ruby Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp2/*.def
+$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp2/*.def
 RESULT3=$?
 kill $PROCID
 
