248886b86f0b52bc4f45e91792eccbea9091e4f6
Ambal <pogrebniak@gala.net>
2014-03-09 12:08:40 +0200

    Update FIELD_SET macro with getIfSet() function

--- src/C++/FieldMap.h
+++ src/C++/FieldMap.h
@@ -229,7 +229,9 @@ bool isSet( const FIELD& field ) const    \
 void set( const FIELD& field )            \
 { (MAP).setField(field); }                \
 FIELD& get( FIELD& field ) const          \
-{ return (FIELD&)(MAP).getField(field); }
+{ return (FIELD&)(MAP).getField(field); } \
+bool getIfSet( FIELD& field ) const       \
+{ return (MAP).getFieldIfSet(field); }
 
 #define FIELD_GET_PTR( MAP, FLD ) \
 (const FIX::FLD*)MAP.getFieldPtr( FIX::FIELD::FLD )
