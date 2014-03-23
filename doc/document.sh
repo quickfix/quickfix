9cfa4c8f8538954bf17a67588d580bfd990e23e4
Oren Miller <omiller@lmp-omiller01.hsd1.il.comcast.net>
2014-03-23 02:12:15 -0500

    Run tidy on html

--- doc/document.sh
+++ doc/document.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+WHICH_TIDY=$(which tidy)
+if [ $? == 0 ] ; then
+    find html "*.html" | xargs -n1 tidy -config tidy.config -m
+fi
+
 WHICH_DOXYGEN=$(which doxygen)
 if [ $? == 0 ] ; then
     doxygen
