b42ee042ad10330b0876babac9f1648ac0957887
Oren Miller <oren@ubuntu-linux.(none)>
2014-04-12 10:29:24 -0500

    All FIX50 sessions for acceptance tests run in a single FIXT transport under linux

--- test/runat.sh
+++ test/runat.sh
@@ -10,23 +10,8 @@ PORT=$1
 ./at -f cfg/at.cfg &
 PROCID=$!
 cd $DIR
-$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix4*/*.def definitions/server/fix50/*.def
-RESULT1=$?
-kill $PROCID
+$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix4*/*.def definitions/server/fix50/*.def definitions/server/fix50sp1/*.def definitions/server/fix50sp2/*.def
 
-./at -f cfg/atsp1.cfg &
-PROCID=$!
-cd $DIR
-$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp1/*.def
-RESULT2=$?
+RESULT=$?
 kill $PROCID
-
-./at -f cfg/atsp2.cfg &
-PROCID=$!
-cd $DIR
-$RUBY Runner.rb 127.0.0.1 $PORT definitions/server/fix50sp2/*.def
-RESULT3=$?
-kill $PROCID
-
-RESULT=$(( $RESULT1 + $RESULT2 + $RESULT3 ))
 exit $RESULT
