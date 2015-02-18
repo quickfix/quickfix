612f5c8898f573e91cc16b44b639e02169970934
Wellington Cordeiro <wellington@wellingtoncordeiro.com>
2015-02-18 15:03:25 -0700

    Remove use of builtin, as it causes errors.

--- src/python/swig.sh
+++ src/python/swig.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-swig -I../C++ -threads -python -py3 -c++ -builtin -o QuickfixPython.cpp quickfix.i
+swig -I../C++ -threads -python -py3 -c++ -o QuickfixPython.cpp quickfix.i
 cat quickfix_fields.py >> quickfix.py
