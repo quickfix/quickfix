a015ad06c0899c9cc4833da31fefca0bafb5e8a4
Karakus, Murat <muratkarakus7@gmail.com>
2016-10-23 14:23:44 +0800

    Improve mysql db populate script
    It is not clear from the error message that script requires username and
    password. Proper error handling is done and caller is informed with the script
    usage.

--- src/sql/mysql/create.sh
+++ src/sql/mysql/create.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+LOG_TAG="[MYSQL_DB_CREATE]"
+STATUS_SUCCESS="[SUCCESS]"
+STATUS_ERROR="[ERROR]"
+EXIT_ERROR=1
+EXIT_SUCCESS=0
+
+# Check usage
+if [ "$#" -ne 2 ]; then
+  echo "$LOG_TAG $STATUS_ERROR : Illegal number of parameters"
+  echo "Usage: $0 <mysql username> <mysql password>"
+  exit EXIT_ERROR
+fi
+
 DB_USER=$1
 DB_PASS=$2
 
 echo Creating database
 mysql -u${DB_USER} -p${DB_PASS} --execute="source mysql.sql";
-
+exit EXIT_SUCCESS
