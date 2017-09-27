15fc8f8449384c41e32c1e36e5bfff4c10ee4404
Eric Bonilla <ericcbonilla@gmail.com>
2017-09-27 09:20:52 -0700

    Modified messages table to use auto-incrementing primary key, timestamp, unique together constraint

--- src/sql/postgresql/messages_table.sql
+++ src/sql/postgresql/messages_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE messages (
+  id SERIAL PRIMARY KEY,
+  created_at timestamp default now(),
   beginstring CHAR(8) NOT NULL,
   sendercompid VARCHAR(64) NOT NULL,
   targetcompid VARCHAR(64) NOT NULL,
   session_qualifier VARCHAR(64) NOT NULL,
-  msgseqnum INTEGER NOT NULL, 
+  msgseqnum INTEGER NOT NULL,
   message TEXT NOT NULL,
-  PRIMARY KEY (beginstring, sendercompid, targetcompid, session_qualifier, msgseqnum)
+  UNIQUE (created_at, sendercompid, targetcompid, session_qualifier, msgseqnum)
 );
