28e0fee07acae5d4bfe1466cf56c91bc1fdb2fb3
Jon Cinque <jon.cinque@gmail.com>
2017-11-05 15:58:21 -0500

    Fix test file names for python3 testing

--- src/python/test-py3.sh
+++ src/python/test-py3.sh
@@ -3,8 +3,8 @@ export LD_LIBRARY_PATH=../../src/C++/.libs
 export DYLD_LIBRARY_PATH=../../src/C++/.libs
 export PYTHONPATH=../../lib/python
 
-python3 test/DataDictionaryTestCase-py3.py
-python3 test/DictionaryTestCase-py3.py
-python3 test/FieldBaseTestCase-py3.py
-python3 test/SessionSettingsTestCase-py3.py
-python3 test/MessageTestCase-py3.py
+python3 test/DataDictionaryTestCase.py
+python3 test/DictionaryTestCase.py
+python3 test/FieldBaseTestCase.py
+python3 test/SessionSettingsTestCase.py
+python3 test/MessageTestCase.py
