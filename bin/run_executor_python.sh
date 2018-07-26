f6924905bf260432be4b145d9aa459278524eb5e
murphypa <poobalan-arumugam@users.noreply.github.com>
2018-07-26 06:14:49 +0000

    run_executor_python.sh: default python version
    point to correct quickfix python lib by getting version from python in path
    in case that is specified by a venv or as provided by the operating system.

--- bin/run_executor_python.sh
+++ bin/run_executor_python.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export PYTHONPATH=../lib/python
+DEFAULTPYTHONVERSION="$(python -c 'import sys; print(sys.version_info.major);')"
+export PYTHONPATH="../lib/python${DEFAULTPYTHONVERSION}"
 
 python ../examples/executor/python/executor.py cfg/executor.cfg
