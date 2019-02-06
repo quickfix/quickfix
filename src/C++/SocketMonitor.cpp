74af86f549e809b396191178812f1d26d47a36f3
Lefteris Eleftheriades <leleftheriades@trading-point.com>
2019-02-06 13:05:09 +0200

    Updated SocketMonitor.h and SocketMonitor.cpp to use socket_handle instead of int

--- src/C++/SocketMonitor.cpp
+++ src/C++/SocketMonitor.cpp
@@ -37,7 +37,7 @@ SocketMonitor::SocketMonitor( int timeout )
 {
   socket_init();
 
-  std::pair<int, int> sockets = socket_createpair();
+  std::pair<socket_handle, socket_handle> sockets = socket_createpair();
   m_signal = sockets.first;
   m_interrupt = sockets.second;
   socket_setnonblock( m_signal );
@@ -62,7 +62,7 @@ SocketMonitor::~SocketMonitor()
   socket_term();
 }
 
-bool SocketMonitor::addConnect( int s )
+bool SocketMonitor::addConnect(socket_handle s )
 {
   socket_setnonblock( s );
   Sockets::iterator i = m_connectSockets.find( s );
@@ -72,7 +72,7 @@ bool SocketMonitor::addConnect( int s )
   return true;
 }
 
-bool SocketMonitor::addRead( int s )
+bool SocketMonitor::addRead(socket_handle s )
 {
   socket_setnonblock( s );
   Sockets::iterator i = m_readSockets.find( s );
@@ -82,7 +82,7 @@ bool SocketMonitor::addRead( int s )
   return true;
 }
 
-bool SocketMonitor::addWrite( int s )
+bool SocketMonitor::addWrite(socket_handle s )
 {
   if( m_readSockets.find(s) == m_readSockets.end() )
     return false;
@@ -95,7 +95,7 @@ bool SocketMonitor::addWrite( int s )
   return true;
 }
 
-bool SocketMonitor::drop( int s )
+bool SocketMonitor::drop(socket_handle s )
 {
   Sockets::iterator i = m_readSockets.find( s );
   Sockets::iterator j = m_writeSockets.find( s );
@@ -165,12 +165,12 @@ bool SocketMonitor::sleepIfEmpty( bool poll )
     return false;
 }
 
-void SocketMonitor::signal( int socket )
+void SocketMonitor::signal(socket_handle socket )
 {
   socket_send( m_signal, (char*)&socket, sizeof(socket) );
 }
 
-void SocketMonitor::unsignal( int s )
+void SocketMonitor::unsignal(socket_handle s )
 {
   Sockets::iterator i = m_writeSockets.find( s );
   if( i == m_writeSockets.end() ) return;
@@ -229,10 +229,10 @@ void SocketMonitor::processReadSet( Strategy& strategy, fd_set& readSet )
 #ifdef _MSC_VER
   for ( unsigned i = 0; i < readSet.fd_count; ++i )
   {
-    int s = readSet.fd_array[ i ];
+    socket_handle s = readSet.fd_array[ i ];
     if( s == m_interrupt )
     {
-      int socket = 0;
+      socket_handle socket = 0;
       socket_recv( s, (char*)&socket, sizeof(socket) );
       addWrite( socket );
     }
@@ -246,12 +246,12 @@ void SocketMonitor::processReadSet( Strategy& strategy, fd_set& readSet )
     Sockets sockets = m_readSockets;
     for ( i = sockets.begin(); i != sockets.end(); ++i )
     {
-      int s = *i;
+      socket_handle s = *i;
       if ( !FD_ISSET( *i, &readSet ) )
         continue;
       if( s == m_interrupt )
       {
-        int socket = 0;
+        socket_handle socket = 0;
         socket_recv( s, (char*)&socket, sizeof(socket) );
         addWrite( socket );
       }
@@ -268,7 +268,7 @@ void SocketMonitor::processWriteSet( Strategy& strategy, fd_set& writeSet )
 #ifdef _MSC_VER
   for ( unsigned i = 0; i < writeSet.fd_count; ++i )
   {
-    int s = writeSet.fd_array[ i ];
+    socket_handle s = writeSet.fd_array[ i ];
     if( m_connectSockets.find(s) != m_connectSockets.end() )
     {
       m_connectSockets.erase( s );
@@ -285,7 +285,7 @@ void SocketMonitor::processWriteSet( Strategy& strategy, fd_set& writeSet )
   Sockets sockets = m_connectSockets;
   for( i = sockets.begin(); i != sockets.end(); ++i )
   {
-    int s = *i;
+    socket_handle s = *i;
     if ( !FD_ISSET( *i, &writeSet ) )
       continue;
     m_connectSockets.erase( s );
@@ -296,7 +296,7 @@ void SocketMonitor::processWriteSet( Strategy& strategy, fd_set& writeSet )
   sockets = m_writeSockets;
   for( i = sockets.begin(); i != sockets.end(); ++i )
   {
-    int s = *i;
+    socket_handle s = *i;
     if ( !FD_ISSET( *i, &writeSet ) )
       continue;
     strategy.onWrite( *this, s );
@@ -309,7 +309,7 @@ void SocketMonitor::processExceptSet( Strategy& strategy, fd_set& exceptSet )
 #ifdef _MSC_VER
   for ( unsigned i = 0; i < exceptSet.fd_count; ++i )
   {
-    int s = exceptSet.fd_array[ i ];
+    socket_handle s = exceptSet.fd_array[ i ];
     strategy.onError( *this, s );
   }
 #else
@@ -317,7 +317,7 @@ void SocketMonitor::processExceptSet( Strategy& strategy, fd_set& exceptSet )
     Sockets sockets = m_connectSockets;
     for ( i = sockets.begin(); i != sockets.end(); ++i )
     {
-      int s = *i;
+      socket_handle s = *i;
       if ( !FD_ISSET( *i, &exceptSet ) )
         continue;
       strategy.onError( *this, s );
--- src/C++/SocketMonitor.h
+++ src/C++/SocketMonitor.h
@@ -41,6 +41,8 @@ typedef int socklen_t;
 #include <queue>
 #include <time.h>
 
+#include "Utility.h"
+
 namespace FIX
 {
 /// Monitors events on a collection of sockets.
@@ -52,20 +54,20 @@ public:
   SocketMonitor( int timeout = 0 );
   virtual ~SocketMonitor();
 
-  bool addConnect( int socket );
-  bool addRead( int socket );
-  bool addWrite( int socket );
-  bool drop( int socket );
-  void signal( int socket );
-  void unsignal( int socket );
+  bool addConnect(socket_handle socket );
+  bool addRead(socket_handle socket );
+  bool addWrite(socket_handle socket );
+  bool drop(socket_handle socket );
+  void signal(socket_handle socket );
+  void unsignal(socket_handle socket );
   void block( Strategy& strategy, bool poll = 0, double timeout = 0.0 );
 
   size_t numSockets() 
   { return m_readSockets.size() - 1; }
 
 private:
-  typedef std::set < int > Sockets;
-  typedef std::queue < int > Queue;
+  typedef std::set < socket_handle > Sockets;
+  typedef std::queue < socket_handle > Queue;
 
   void setsockopt();
   bool bind();
@@ -84,8 +86,8 @@ private:
   clock_t m_ticks;
 #endif
 
-  int m_signal;
-  int m_interrupt;
+  socket_handle m_signal;
+  socket_handle m_interrupt;
   Sockets m_connectSockets;
   Sockets m_readSockets;
   Sockets m_writeSockets;
@@ -97,10 +99,10 @@ public:
   public:
     virtual ~Strategy()
     {}
-    virtual void onConnect( SocketMonitor&, int socket ) = 0;
-    virtual void onEvent( SocketMonitor&, int socket ) = 0;
-    virtual void onWrite( SocketMonitor&, int socket ) = 0;
-    virtual void onError( SocketMonitor&, int socket ) = 0;
+    virtual void onConnect( SocketMonitor&, socket_handle socket ) = 0;
+    virtual void onEvent( SocketMonitor&, socket_handle socket ) = 0;
+    virtual void onWrite( SocketMonitor&, socket_handle socket ) = 0;
+    virtual void onError( SocketMonitor&, socket_handle socket ) = 0;
     virtual void onError( SocketMonitor& ) = 0;
     virtual void onTimeout( SocketMonitor& )
   {}}
