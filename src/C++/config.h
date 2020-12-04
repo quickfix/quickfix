f72195e50a3221e9a3f866c929e15698debb3c7e
Oren Miller <orenmiller@gmail.com>
2020-12-04 02:19:04 -0600

    /wip

---
+++ src/C++/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// Define if you are using stlport
+// #define HAVE_STLPORT
+
+// Define if you have sql library (libMySQL.lib)
+// #define HAVE_MYSQL 1
+
+// Define if you have sql library (libpq.lib)
+// #define HAVE_POSTGRESQL 1
+
+// Define if you have odbc library (Odbc32.lib)
+// #define HAVE_ODBC 1
+
+// Define if you have ftime
+#define HAVE_FTIME 1
+
+// Define if you have openssl
+// #define HAVE_SSL 1
