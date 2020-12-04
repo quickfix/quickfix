e0dbe149eef98320f1fe1b07845394b9c5d4c90a
Oren Miller <oren@quickfixengine.org>
2020-12-03 21:42:26 -0600

    Always include config do build defines are pulled in

--- src/stdafx.h
+++ src/stdafx.h
@@ -13,7 +13,7 @@
 #define WIN32_LEAN_AND_MEAN// Exclude rarely-used stuff from Windows headers
 
 #include <stdio.h>
-#include "config_windows.h"
+#include "config.h"
 
 // TODO: reference additional headers your program requires here
 
