9375a54b543dadb2db25d754378b93acc57c7a7a
Oren Miller <oren@quickfixengine.org>
2021-01-16 17:07:12 -0600

    /wip

--- src/C++/config.h
+++ src/C++/config.h
@@ -1,19 +1,11 @@
-#pragma once
+#ifndef CONFIG_H_IN
+#define CONFIG_H_IN
 
-// Define if you are using stlport
-// #define HAVE_STLPORT
+/* #undef HAVE_EMX */
+#define HAVE_CXX17
+#define HAVE_STD_SHARED_PTR
+/* #undef HAVE_SHARED_PTR_IN_TR1_NAMESPACE */
+/* #undef HAVE_STD_TR1_SHARED_PTR_FROM_TR1_MEMORY_HEADER */
+#define HAVE_STD_UNIQUE_PTR
 
-// Define if you have sql library (libMySQL.lib)
-// #define HAVE_MYSQL 1
-
-// Define if you have sql library (libpq.lib)
-// #define HAVE_POSTGRESQL 1
-
-// Define if you have odbc library (Odbc32.lib)
-// #define HAVE_ODBC 1
-
-// Define if you have ftime
-#define HAVE_FTIME 1
-
-// Define if you have openssl
-// #define HAVE_SSL 1
+#endif
