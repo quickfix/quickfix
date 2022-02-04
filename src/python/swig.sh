3feb57033e9f43684be1a0c23381f302c6c66f2f
Giovanni Opromolla <giopromolla@gmail.com>
2022-02-04 14:27:46 -0300

    ssl

--- src/python/swig.sh
+++ src/python/swig.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-swig -I../C++ -threads -python -c++ -o QuickfixPython.cpp quickfix.i
+swig -I../C++ -threads -D__cpp_noexcept_function_type -DHAVE_SSL=1 -python -c++ -o QuickfixPython.cpp quickfix.i
 cat quickfix_fields.py >> quickfix.py
 sed -i "s/object.__getattr__/object.__getattribute__/g" quickfix.py
