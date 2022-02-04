e43365230ba8a7ac0230f0d4b4e10f884d710283
Giovanni Opromolla <giopromolla@gmail.com>
2022-02-04 19:48:55 -0300

    tests

--- setup.py
+++ setup.py
@@ -1,10 +1,44 @@
 from distutils.core import setup
 from distutils.core import Extension
+from distutils.command.install import install
+from distutils.command.build import build
+from distutils.command.build_ext import build_ext
 from distutils.sysconfig import get_config_vars
 
+import subprocess
+import shutil
 import glob
+import os
 import sys
 
+class build_ext_subclass( build_ext ):
+    def build_extensions(self):
+        self.compiler.define_macro("PYTHON_MAJOR_VERSION", sys.version_info[0])
+        print("Testing for std::tr1::shared_ptr...")
+        try:
+            self.compiler.compile(['test_std_tr1_shared_ptr.cpp'])
+            self.compiler.define_macro("HAVE_STD_TR1_SHARED_PTR")
+            print("...found")
+        except:
+            print(" ...not found")
+
+        print("Testing for std::shared_ptr...")
+        try:
+            self.compiler.compile(['test_std_shared_ptr.cpp'], extra_preargs=['-std=c++0x']),
+            self.compiler.define_macro("HAVE_STD_SHARED_PTR")
+            print("...found")
+        except:
+            print("...not found")
+
+        print("Testing for std::unique_ptr...")
+        try:
+            self.compiler.compile(['test_std_unique_ptr.cpp'], extra_preargs=['-std=c++0x']),
+            self.compiler.define_macro("HAVE_STD_UNIQUE_PTR")
+            print("...found")
+        except:
+            print("...not found")
+    
+        build_ext.build_extensions(self)
 
 # Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
 import distutils.sysconfig
@@ -41,5 +75,6 @@ setup(name='quickfix',
       download_url='http://www.quickfixengine.org',
       license=license,
       include_dirs=['src/C++',],
+      cmdclass = {'build_ext': build_ext_subclass },
       ext_modules=[Extension('_quickfix', glob.glob('src/C++/*.cpp'), extra_compile_args=['-std=c++0x', '-Wno-deprecated', '-Wno-unused-variable', '-Wno-deprecated-declarations', '-Wno-maybe-uninitialized'])],
 )
