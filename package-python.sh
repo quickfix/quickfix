b0ab3d9a1c6e2326b4ef2c065ee26efa4021fd6c
Travis Randolph <travisR004@gmail.com>
2022-02-04 16:23:33 -0800

    No idea at this point

---
+++ package-python.sh
@@ -0,0 +1,28 @@
+rm -rf quickfix-python/C++
+rm -rf quickfix-python/spec
+rm -rf quickfix-python/quickfix*.py
+rm -rf quickfix-python/doc
+rm -rf quickfix-python/LICENSE
+
+mkdir quickfix-python/C++
+mkdir quickfix-python/spec
+
+cp quickfix/LICENSE quickfix-python
+
+cp quickfix/src/python3/*.py quickfix-python
+cp quickfix/src/C++/*.h quickfix-python/C++
+cp quickfix/src/C++/*.hpp quickfix-python/C++
+cp quickfix/src/C++/*.cpp quickfix-python/C++
+cp -R quickfix/src/C++/double-conversion quickfix-python/C++
+cp quickfix/src/python3/QuickfixPython.cpp quickfix-python/C++
+cp quickfix/src/python3/QuickfixPython.h quickfix-python/C++
+
+cp quickfix/spec/FIX*.xml quickfix-python/spec
+
+touch quickfix-python/C++/config.h
+touch quickfix-python/C++/config_windows.h
+rm -f quickfix-python/C++/stdafx.*
+
+pushd quickfix-python
+python setup.py sdist
+PYTHONWARNINGS="ignore" twine upload --repository-url https://test.pypi.org/legacy/ dist/*
--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ import glob
 import os
 import sys
 
-class build_ext_subclass( build_ext ):
+
+class build_ext_subclass(build_ext):
     def build_extensions(self):
         self.compiler.define_macro("PYTHON_MAJOR_VERSION", sys.version_info[0])
         print("Testing for std::tr1::shared_ptr...")
@@ -24,7 +25,8 @@ class build_ext_subclass( build_ext ):
 
         print("Testing for std::shared_ptr...")
         try:
-            self.compiler.compile(['test_std_shared_ptr.cpp'], extra_preargs=['-std=c++0x']),
+            self.compiler.compile(['test_std_shared_ptr.cpp'],
+                                  extra_preargs=['-std=c++0x']),
             self.compiler.define_macro("HAVE_STD_SHARED_PTR")
             print("...found")
         except:
@@ -32,39 +34,33 @@ class build_ext_subclass( build_ext ):
 
         print("Testing for std::unique_ptr...")
         try:
-            self.compiler.compile(['test_std_unique_ptr.cpp'], extra_preargs=['-std=c++0x']),
+            self.compiler.compile(['test_std_unique_ptr.cpp'],
+                                  extra_preargs=['-std=c++0x']),
             self.compiler.define_macro("HAVE_STD_UNIQUE_PTR")
             print("...found")
         except:
             print("...not found")
-    
+
         build_ext.build_extensions(self)
+
 
 # Remove the "-Wstrict-prototypes" compiler option, which isn't valid for C++.
 import distutils.sysconfig
+
 cfg_vars = distutils.sysconfig.get_config_vars()
 for key, value in cfg_vars.items():
     if type(value) == str:
         cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
-        
-long_description=''
+
+long_description = ''
 with open('LICENSE') as file:
     license = file.read();
 
 setup(name='quickfix',
-      version='1.15.2',
-      py_modules=[
-        'src/python/quickfix',
-        'src/python/quickfixt11', 
-        'src/python/quickfix40', 
-        'src/python/quickfix41', 
-        'src/python/quickfix42', 
-        'src/python/quickfix43', 
-        'src/python/quickfix44', 
-        'src/python/quickfix50', 
-        'src/python/quickfix50sp1', 
-        'src/python/quickfix50sp2'
-      ],
+      version='1.15.1',
+      py_modules=['quickfix', 'quickfixt11', 'quickfix40', 'quickfix41',
+                  'quickfix42', 'quickfix43', 'quickfix44', 'quickfix50',
+                  'quickfix50sp1', 'quickfix50sp2'],
       data_files=[('share/quickfix', glob.glob('spec/FIX*.xml'))],
       author='Oren Miller',
       author_email='oren@quickfixengine.org',
@@ -74,7 +70,12 @@ setup(name='quickfix',
       url='http://www.quickfixengine.org',
       download_url='http://www.quickfixengine.org',
       license=license,
-      include_dirs=['src/C++'],
+      include_dirs=['C++'],
       cmdclass={'build_ext': build_ext_subclass},
-      ext_modules=[Extension('_quickfix', glob.glob('src/C++/*.cpp'), extra_compile_args=['-std=c++0x', '-Wno-deprecated', '-Wno-unused-variable', '-Wno-deprecated-declarations', '-Wno-maybe-uninitialized'])],
-)
+      ext_modules=[Extension('_quickfix', glob.glob('C++/*.cpp'),
+                             extra_compile_args=['-std=c++0x',
+                                                 '-Wno-deprecated',
+                                                 '-Wno-unused-variable',
+                                                 '-Wno-deprecated-declarations',
+                                                 '-Wno-maybe-uninitialized'])],
+      )
