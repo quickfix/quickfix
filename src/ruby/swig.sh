10ee70c25fe4c7025580fc66ca0d20cb8e8b4710
Oren Miller <oren@quickfixengine.org>
2023-07-05 19:59:59 -0500

    Generate ruby with HAVE_SSL

--- src/ruby/swig.sh
+++ src/ruby/swig.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swig -I../C++ -D__cpp_noexcept_function_type -ruby -c++ -o QuickfixRuby.cpp quickfix.i
+swig -I../C++ -D__cpp_noexcept_function_type -ruby -DHAVE_SSL=1 -c++ -o QuickfixRuby.cpp quickfix.i
