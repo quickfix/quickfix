77e9903d92aa10adf38536cb3fd8b8112b4c877b
Oren Miller <oren@quickfixengine.org>
2024-06-30 18:03:05 -0500

    Create postgressql user

--- src/sql/mysql/create_user.sh
+++ src/sql/mysql/create_user.sh
@@ -4,5 +4,5 @@ DB_USER=$1
 DB_PASS=$2
 
 echo Creating quickfix user
-mysql -u${DB_USER} -p${DB_PASS} --execute="source quickfix_user.sql";
+mysql -u${DB_USER} -p${DB_PASS} --execute="source user.sql";
 
--- src/sql/mysql/user.sql
+++ src/sql/mysql/user.sql
@@ -1,2 +1,2 @@
-CREATE USER 'quickfix'@'localhost';
+CREATE USER IF NOT EXISTS 'quickfix'@'localhost';
 GRANT INSERT, UPDATE, DELETE, SELECT ON quickfix.* TO 'quickfix'@'localhost';
