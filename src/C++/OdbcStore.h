b345eeebc2ba3d77c5fc04f5948f071ae3255770
Oren Miller <oren@quickfixengine.org>
2024-06-30 22:19:08 -0500

    ODBC compiles with SEQNUM

--- src/C++/OdbcStore.h
+++ src/C++/OdbcStore.h
@@ -85,13 +85,13 @@ public:
              const std::string& connectionString );
   ~OdbcStore();
 
-  bool set( int, const std::string& ) EXCEPT ( IOException );
-  void get( int, int, std::vector < std::string > & ) const EXCEPT ( IOException );
+  bool set( SEQNUM, const std::string& ) EXCEPT ( IOException );
+  void get( SEQNUM, SEQNUM, std::vector < std::string > & ) const EXCEPT ( IOException );
 
-  int getNextSenderMsgSeqNum() const EXCEPT ( IOException );
-  int getNextTargetMsgSeqNum() const EXCEPT ( IOException );
-  void setNextSenderMsgSeqNum( int value ) EXCEPT ( IOException );
-  void setNextTargetMsgSeqNum( int value ) EXCEPT ( IOException );
+  SEQNUM getNextSenderMsgSeqNum() const EXCEPT ( IOException );
+  SEQNUM getNextTargetMsgSeqNum() const EXCEPT ( IOException );
+  void setNextSenderMsgSeqNum( SEQNUM value ) EXCEPT ( IOException );
+  void setNextTargetMsgSeqNum( SEQNUM value ) EXCEPT ( IOException );
   void incrNextSenderMsgSeqNum() EXCEPT ( IOException );
   void incrNextTargetMsgSeqNum() EXCEPT ( IOException );
 
