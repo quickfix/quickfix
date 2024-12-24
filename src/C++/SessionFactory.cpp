45e9fc1cad73ed89629dda6a1b70d53df65e7e8d
bronzeboyvn <bronzeboyvn@yahoo.com>
2024-12-25 06:42:57 +0700

    MillisecondsInTimeStamp=N config (#622)
    * MillisecondsInTimeStamp=N config
    
    * using setMillisecondsInTimeStamp function

--- src/C++/SessionFactory.cpp
+++ src/C++/SessionFactory.cpp
@@ -201,7 +201,7 @@ Session* SessionFactory::create( const SessionID& sessionID,
   if ( settings.has( REFRESH_ON_LOGON ) )
     pSession->setRefreshOnLogon( settings.getBool( REFRESH_ON_LOGON ) );
   if ( settings.has( MILLISECONDS_IN_TIMESTAMP ) )
-    pSession->setTimestampPrecision(3);
+    pSession->setMillisecondsInTimeStamp( settings.getBool( MILLISECONDS_IN_TIMESTAMP ) );
   if ( settings.has( TIMESTAMP_PRECISION ) )
     pSession->setTimestampPrecision(settings.getInt( TIMESTAMP_PRECISION ) );
   if ( settings.has( PERSIST_MESSAGES ) )
