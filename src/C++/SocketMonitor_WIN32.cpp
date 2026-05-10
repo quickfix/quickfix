73fc09e18e77f8280c90fcc84ce532b3e5693fcf
Oren Miller <oren@quickfixengine.org>
2026-05-10 15:58:16 +0100

    Apply clang-format to SocketMonitor_WIN32.cpp

--- src/C++/SocketMonitor_WIN32.cpp
+++ src/C++/SocketMonitor_WIN32.cpp
@@ -197,14 +197,19 @@ void SocketMonitor::block(Strategy &strategy, bool poll, double timeout) {
   {
     Sockets pending = m_connectSockets;
     for (socket_handle s : pending) {
-      if (m_connectSockets.count(s) == 0) continue;
+      if (m_connectSockets.count(s) == 0) {
+        continue;
+      }
       int error = 0, len = sizeof(error);
       getsockopt(s, SOL_SOCKET, SO_ERROR, (char *)&error, &len);
-      if (error)
+      if (error) {
         strategy.onError(*this, s);
+      }
     }
   }
-  if (!m_dropped.empty()) return;
+  if (!m_dropped.empty()) {
+    return;
+  }
 
   fd_set readSet;
   FD_ZERO(&readSet);
@@ -230,7 +235,9 @@ void SocketMonitor::block(Strategy &strategy, bool poll, double timeout) {
     {
       Sockets pending = m_connectSockets;
       for (socket_handle s : pending) {
-        if (!m_connectSockets.count(s)) continue;
+        if (!m_connectSockets.count(s)) {
+          continue;
+        }
         int error = 0, len = sizeof(error);
         getsockopt(s, SOL_SOCKET, SO_ERROR, (char *)&error, &len);
         if (error) {
